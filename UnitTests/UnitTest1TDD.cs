ce5f8689452e06b19f39ebe8c0eb2a9b8e798662
co2222222 <108412930+co2222222@users.noreply.github.com>
2022-07-07 19:41:33 +0100

    Add files via upload

---
+++ UnitTests/UnitTest1TDD.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1TDD
+    {
+        [TestMethod]
+        public void AddTest()
+        {
+            ELEducationSample.Calculator c = new ELEducationSample.Calculator();
+            int res = c.Add(40, 10);
+            Assert.AreEqual(res, 50);
+            string result = c.DebugWrite(res);
+            Assert.AreEqual(result, res.ToString());
+        }
+        [TestMethod]
+        public void SubtractTest()
+        {
+            ELEducationSample.Calculator c = new ELEducationSample.Calculator();
+            int res = c.Subtract(20, 10);
+            Assert.AreEqual(res, 10);
+            string result = c.DebugWrite(res);
+            Assert.AreEqual(result, res.ToString());
+        }
+        [TestMethod]
+        public void DivideTest()
+        {
+            ELEducationSample.Calculator c = new ELEducationSample.Calculator();
+            int res = c.Divide(50, 5);
+            Assert.AreEqual(res, 10);
+            string result = c.DebugWrite(res);
+            Assert.AreEqual(result, res.ToString());
+        }
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            ELEducationSample.Calculator c = new ELEducationSample.Calculator();
+            int res = c.Multiply(10, 5);
+            Assert.AreEqual(res, 50);
+            string result = c.DebugWrite(res);
+            Assert.AreEqual(result, res.ToString());
+        }
+    }
+}
---
+++ UnitTests/UnitTest2Mock.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    public class MockCalc : ELEducationSample.IDebugWrite
+    {
+        public String DebugWrite(int result)
+        {
+            return result.ToString();
+        }
+
+        public void DebugWriteToDB(string operation, int result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [TestClass]
+    public class UnitTest2Mock
+    {
+        [TestMethod]
+        public void TestDebugMethodWithMock()
+        {
+            MockCalc mock = new MockCalc();
+            string result = mock.DebugWrite(2);
+            Assert.AreEqual(result, 2.ToString());
+        }
+
+    }
+}
